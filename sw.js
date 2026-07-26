7186221eac37a36e4e1e69d442ad4d97ef71499a
Sound News <victorchitive1@gmail.com>
2026-07-27 00:21:33 +0200

    confirmo

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'soundnews-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './capa.jpg'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Ativação e limpeza de cache antigo
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceção de pedidos
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
